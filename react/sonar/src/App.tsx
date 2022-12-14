459af657371a2312b118f4c7de956832640072e9
Rahul Vuppala <55148380+Rahulvvsv@users.noreply.github.com>
2022-12-14 11:52:22 +0530

    sonar

---
+++ react/sonar/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Testing/Headers/Headers';
+import AddInput from "./AddInput/AddInput"
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header title="Hello world"></Header>
+        {/* <AddInput></AddInput> */}
+    </div>
+  );
+}
+
+export default App;
